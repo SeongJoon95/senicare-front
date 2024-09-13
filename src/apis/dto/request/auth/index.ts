817c6db01d3b3803700476b1e337f0104a983bdb
SeongJoon95 <gnsxmtkfkd@naver.com>
2024-09-13 17:42:53 +0900

    2024-09-13 17:42

--- src/apis/dto/request/auth/index.ts
+++ src/apis/dto/request/auth/index.ts
@@ -6,10 +6,10 @@ import TelAuthCheckRequestDto from "./tel-auth-check.request.dto";
 
 //  원래는 export만 사용하면 되지만 구현되지 않은 인터페이스에 대해서는 ' export type '으로 다시 내보내야 한다.
 export type {
-    SignInRequestDto, // 로그인 DTO
-    SignUpRequestDto, // 회원가입 DTO
-    IdCheckRequestDto,
-    TelAuthRequestDto,
-    TelAuthCheckRequestDto
+    SignInRequestDto, // 로그인 
+    SignUpRequestDto, // 회원가입 
+    IdCheckRequestDto, // 아이디 중복확인
+    TelAuthRequestDto, // 전화번호 중복확인
+    TelAuthCheckRequestDto // 인증번호 확인
     
 }
