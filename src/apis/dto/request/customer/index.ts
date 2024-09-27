be263de80b4c5afe2373df57af033c7265968dde
SeongJoon95 <gnsxmtkfkd@naver.com>
2024-09-27 17:39:31 +0900

    2024-09-27 17:40

---
+++ src/apis/dto/request/customer/index.ts
@@ -0,0 +1,6 @@
+import PostCustomerRequestDto from "./post-customer.request.dto";
+import PatchCustomerRequestDto from "./patch-customer.request.dto";
+export type{
+    PostCustomerRequestDto,
+    PatchCustomerRequestDto
+}
